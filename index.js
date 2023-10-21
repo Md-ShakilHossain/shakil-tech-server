e30191f902b34632a29b7363daae573ba52ed1d2
Md-ShakilHossain <shakilh550@gmail.com>
2023-10-21 21:37:36 +0600

    aassignment complete

--- index.js
+++ index.js
@@ -38,42 +38,86 @@ async function run() {
       res.send(result);
     });
 
-    app.get('/product/:id', async(req, res) => {
+    app.get('/product/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const product = await productsCollection.findOne(query);
       res.send(product);
     });
 
     app.get('/products/:brandName', async (req, res) => {
       const bName = req.params.brandName;
-      const query = { brandName : bName };
+      const query = { brandName: bName };
       const cursor = productsCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
     });
 
-    app.get('/carts', async (req, res) => {
-      const cursor = cartCollection.find();
-      const result = await cursor.toArray();
-      res.send(result);
-    })
 
-    
 
     app.post('/products', async (req, res) => {
       const product = req.body;
       console.log(product);
       const result = await productsCollection.insertOne(product);
       res.send(result);
+    });
+
+    app.put('/product/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedInformation = req.body;
+      const information = {
+        $set: {
+          imageURL: updatedInformation.imageURL,
+          name: updatedInformation.name,
+          brandName: updatedInformation.brandName,
+          rating: updatedInformation.rating,
+          price: updatedInformation.price,
+          type: updatedInformation.type
+        }
+      }
+
+      const result = await productsCollection.updateOne(query, information, options);
+      res.send(result);
+
     })
 
-    app.post('/carts', async (req,res) => {
+
+
+
+    // Carts related api
+
+    app.get('/carts', async (req, res) => {
+      const cursor = cartCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log("single id", typeof (id), id);
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.findOne(query);
+      console.log(result);
+      res.send(result);
+    });
+
+
+    app.post('/carts', async (req, res) => {
       const cartData = req.body;
       console.log(cartData);
       const result = await cartCollection.insertOne(cartData);
       res.send(result);
-    })
+    });
+
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id)
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    });
 
 
     // Send a ping to confirm a successful connection
@@ -88,9 +132,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Shakil-Tech server is working perfectly');
+  res.send('Shakil-Tech server is working perfectly');
 })
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`)
+  console.log(`Server is running on port: ${port}`)
 })
